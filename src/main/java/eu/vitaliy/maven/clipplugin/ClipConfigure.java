83e63d24780f8b97ed461bdb9712f6f0cbae68b6
Vitaliy Oliynyk <xaoc00@gmail.com>
2015-02-12 14:30:16 +0100

    handle workspace without pom.xml

--- src/main/java/eu/vitaliy/maven/clipplugin/ClipConfigure.java
+++ src/main/java/eu/vitaliy/maven/clipplugin/ClipConfigure.java
@@ -26,7 +26,7 @@ import java.util.List;
  * example:
  * mvn eu.vitaliy:maven-clip-plugin:0.0.1-SNAPSHOT:clip -Dmodules=module2,module3,module4
  */
-@Mojo( name = "clip", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
+@Mojo( name = "clip", defaultPhase = LifecyclePhase.PROCESS_SOURCES, requiresProject = false)
 public class ClipConfigure extends AbstractMojo{
 
     Log log = getLog();
